b0448c60db04d9c6a68aaf07725a76e24cb0b1aa
nadewd <nadewbdemissew@gmail.com>
2023-09-12 20:11:02 -0700

    Added about page to nav bar & CSS library

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    # Include an .html file extension - unlike when rendering EJS templates
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def all_finches(request):
+    return render(request, 'all-finches.html')
